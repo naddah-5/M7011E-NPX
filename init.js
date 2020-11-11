a03d8b7a69840510366baaa1e60afa3af3a648fd
naddah-5 <andersoskardahl@hotmail.com>
2020-11-11 10:18:57 +0100

    .

--- init.js
+++ init.js
@@ -8,12 +8,16 @@
 
 
 let express = require('express');
-//import Express from express;
+//import express from 'express';
 let app = express();
 let port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Welcome to the homepage.");
+})
+
+app.get("/house", (req, res) => {
+    req()
 })
 
 app.listen(port, () => console.log("Listening on port " + port));
