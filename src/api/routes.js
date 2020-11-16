19bdca36db69cce18c0bd45b64aaf8b8ed18b790
naddah-5 <andersoskardahl@hotmail.com>
2020-11-16 22:00:01 +0100

    .

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+
+router.get('/home', (req, res) => {
+    res.send('Welcom to the homepage!')
+})
+
+module.exports  = router;
