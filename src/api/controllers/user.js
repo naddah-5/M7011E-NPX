d506492a600877e683508374169aca77aca1b8b2
naddah-5 <andersoskardahl@hotmail.com>
2020-12-01 15:56:32 +0100

    .

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,4 +1,7 @@
 const User = require('../models/user');
+let jwt = require('jsonwebtoken');
+
+require('dotenv').config()
 
 
 exports.create = function (req, res, next) {
@@ -16,5 +19,28 @@ exports.create = function (req, res, next) {
       });
   return res.json(result);
   }
+}
+
+exports.login = async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+   
+    if (username == null || password == null){
+        console.log(credentialFail);
+        res.sendStatus(401);
+    }
+    let match = await user.findOne({username: username});
+    let passwordMatch = await bcrypt.compare(password, match.password);
+    if(match && passwordMatch){
+        let token = jwt.sign({
+            username: match.username,
+            manager: match.manager,
+        }, process.env.JWT_ACCESS_TOKEN,{expiresIn: "60m"});
+        // we need to update the cookie with the access token
+    }
+    else{
+        console.log(credentialFail);
+        res.sendStatus(401); //Unauthorized i.e. incorrect 
+    }
 }
 
