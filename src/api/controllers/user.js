24cdf15af666073669c47f32cd1d002ba1f0563b
DilanR <dilanredha@hotmail.com>
2020-12-01 15:45:13 +0100

    Added Post logic for user

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,1 +1,20 @@
-// findOne(username) -> return dB entry relating to username or false
+const User = require('../models/user');
+
+
+exports.create = function (req, res, next) {
+  if(!User.exists({'userName': req.body.userName})) {
+      var user = req.body;
+      //generate salt then hash
+      bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.Hpassword, salt, (err, hash) => {
+          if (err) return next(err);
+          user.Hpassword = hash;
+          next();
+        });
+      });
+  return res.json(result);
+  }
+}
+
